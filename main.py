b4455c863bad51869bffa4bb3b4cb78badc84258
Sara Gorecki <sgorecki@gmail.com>
2020-02-02 22:16:20 -0500

    use arrow keys to move selection cursor

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ class Game:
     def __init__(self):
         self.X = []
         self.O = []
-        self.cursor = (0, 0)
+        self.cursor = [0, 0]
         self.row = 0
         self.column = 0
         self.player = 'X'
 
+    CURSOR_BACKGROUND = '\u001b[42m'
+    BACKGROUND_RESET = '\u001b[0m'
 
     def get_space_value(self):
         if (self.row, self.column) in self.X:
@@ -18,11 +20,12 @@ class Game:
             space = 'O'
         else:
             space = ' '
-        # if row is self.cursor(0)
+        if [self.row, self.column] == self.cursor:
+            space = f'{self.CURSOR_BACKGROUND}{space}{self.BACKGROUND_RESET}'
         return space
 
     def draw_board(self):
-        # print(chr(27) + '[2J' + chr(27) + '[0;0H') # clears terminal
+        print(chr(27) + '[2J' + chr(27) + '[0;0H') # clears terminal
         game_board = f'\n\rPlayer {self.player} Goes:\n\n\r'
 
         while self.row < 3:
@@ -40,26 +43,81 @@ class Game:
             self.column = 0
         
         print(game_board)
+        self.row = 0
+        self.column = 0
+
+    def process_keyboard_input(self):
+        char = ord(sys.stdin.read(1)) # listen for arrow keys and get char code
+        if char == 27: # Arrow keys process as a sequence of three characters
+            next1, next2 = ord(sys.stdin.read(1)), ord(sys.stdin.read(1))
+            if next1 == 91: # The second character in the arrow key sequence
+                if next2 == 65:
+                    return 'up'
+                elif next2 == 66:
+                    return 'down'
+                elif next2 == 67:
+                    return 'right'
+                elif next2 == 68:
+                    return 'left'
+        elif char == 32:
+            return 'space'
+        elif char == 10 or char == 13:
+            return 'enter'
+        elif char == 3: # CTRL-C
+            return 'exit'
+
+    def move_cursor_up(self):
+        if self.cursor[0] > 0:
+            self.cursor[0] -= 1
+
+    def move_cursor_down(self):
+        if self.cursor[0] < 2:
+            self.cursor[0] += 1
+
+    def move_cursor_right(self):
+        if self.cursor[1] < 2:
+            self.cursor[1] += 1
+
+    def move_cursor_left(self):
+        if self.cursor[1] > 0:
+            self.cursor[1] -= 1
+
+    def handle_keypress(self, keypress, mode):
+        if keypress is 'up':
+            self.move_cursor_up()
+        elif keypress is 'down':
+            self.move_cursor_down()
+        elif keypress is 'right':
+            self.move_cursor_right()
+        elif keypress is 'left':
+            self.move_cursor_left()
+        elif keypress is 'enter':
+            tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
+            raise KeyboardInterrupt
+        elif keypress is 'exit':
+            tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
+            raise KeyboardInterrupt
+        return True
 
     def play(self):
         mode = tty.tcgetattr(sys.stdin)
         tty.setraw(sys.stdin)
         accepting_input = True
-
         self.draw_board()
+
+        while accepting_input:
+            keypress = self.process_keyboard_input()
+            accepting_input = self.handle_keypress(keypress, mode)            
+            self.draw_board()
 
 
 def main():
     game = Game()
-    game.play()
 
-    # try:
-    #     board.populate_board()
-    #     while True:
-    #         tick(board)
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     return
+    try:
+        game.play()
+    except KeyboardInterrupt:
+        return
 
 if __name__ == "__main__":
     main()
